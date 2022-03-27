883510dbb8c28151a538440d7c0431e465b1c3c2
Sanches <sanches-zotov@yandex.ru>
2022-03-27 17:25:51 +0300

    add drop-down menu

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-const burger = document.querySelector('.burger');
+const header = document.querySelector('.header');
+const burger = header.querySelector('.burger');
+const nav = header.querySelector('.header__nav');
 burger.addEventListener('click', () => {
     burger.classList.toggle('burger_open');
+    nav.classList.toggle('header__nav_active');
 })
